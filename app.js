768fcb4dd4fd3115b0a4d8db51c5c4f82a268622
MrJones702 <jones.maurice39@yahoo.com>
2022-11-28 22:37:11 -0800

    grid done. aka game layout

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     //all code here will be picked up by script tag in index.html
     
+    const grid= document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+
 })
