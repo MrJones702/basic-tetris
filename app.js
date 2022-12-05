86e3c9dd107404d515055851dc8fdde3d1ca5592
MrJones702 <jones.maurice39@yahoo.com>
2022-12-04 23:03:20 -0800

    button funct? wont pause

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const grid= document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
+    let timerId
+
 
     // The Tetrominoes
     const lTetromino = [
@@ -91,12 +94,14 @@ document.addEventListener('DOMContentLoaded', () => {
     //freeze
     function freeze() {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-        //start a new tetromino falling
-        random = Math.floor(Math.random()* theTetrominoes.length)
-        current = theTetrominoes[random][currentRotaton]
-        currentPosition = 4  
-        draw()
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //start a new tetromino falling
+            random = nextRandom
+            nextRandom = Math.floor(Math.random()* theTetrominoes.length)
+            current = theTetrominoes[random][currentRotaton]
+            currentPosition = 4  
+            draw()
+            displayShape()
         }
     }
 
@@ -139,5 +144,44 @@ document.addEventListener('DOMContentLoaded', () => {
         current = theTetrominoes[random][currentRotaton]
         draw()
     }
+
+    //show up-next tetromino in the mini grid display
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4
+    let displayIndex = 0
+
+    //the tetrominoes without rotations
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //otetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+    ]
+
+    //display shape in the mini-grid
+    function displayShape() {
+        //remove any trace of a tetromino from the entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+            displaySquares[displayIndex + index].classList.add('tetromino')
+        })
+    }
+
+    //add functionality to the button
+    startBtn.addEventListener('click', () => {
+        if (timerId) {
+          clearInterval(timerId)
+          timerId = null
+        } else {
+          draw()
+          timerId = setInterval(moveDown, 1000)
+          nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+          displayShape()
+        }
+      })
+
 
 })
